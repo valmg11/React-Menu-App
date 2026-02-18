dba03ef875037bdab94a8561d39b210accdec515
valmg11 <tictactina05@gmail.com>
2026-02-18 01:03:02 -0500

    app setup

--- menu-app/src/components/MenuApp/index.js
+++ menu-app/src/components/MenuApp/index.js
@@ -1,4 +1,21 @@
-// import "./App.css"
+// import MenuItem from "../MenuItem"
+// const MenuApp = () => {
+//   let menuItem = {         
+//     "itemId": 1,         
+//     "itemPrice": "12",         
+//     "itemName": "Lasagne",         
+//     "itemDescription": "Meat and cheese layered between house-made pasta with bell peppers and onions." 
+//   }
+//   return (
+//     <div className="App">
+//       <MenuItem items={menuItem}></MenuItem>
+//     </div>
+//   )
+// }
+
+// export default MenuApp
+
+import "./App.css"
 import Menu from "../Menu"
 const MenuApp = () => {
   return (
